b1ce6903fef8082b2eb8a3d3b96c7df38734b835
sousa-gabriel <sousa-gabriel@hotmail.com>
2021-08-17 21:33:59 -0300

    started project

---
+++ src/screens/Control/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container, Title} from './styles';
+
+export function Control() {
+  return (
+    <Container>
+      <Title>Painel de Controle</Title>
+    </Container>
+  );
+}
