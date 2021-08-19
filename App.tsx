9448a031de3969593431dff66c0adb883819eef9
sousa-gabriel <sousa-gabriel@hotmail.com>
2021-08-18 22:34:59 -0300

    Finish this Test

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {StorageData} from './src/hooks/useStorageData';
 
 import {StatusBar} from 'react-native';
 import {TabBarRoutes} from './src/routes';
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="light-content" />
-      <TabBarRoutes />
+      <StorageData>
+        <StatusBar barStyle="light-content" />
+        <TabBarRoutes />
+      </StorageData>
     </NavigationContainer>
   );
 }
